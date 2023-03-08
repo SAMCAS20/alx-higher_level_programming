1ba006ed31797e5134b4d09e31afdd68bfa101fc
Samanta Casale <94729493+SAMCAS20@users.noreply.github.com>
2023-03-08 11:23:46 -0800

    9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,3 @@
-ines (6 sloc)  465 Bytes
-
 -- Creates and fills a table second_table with attributes id, name and score.
 CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
